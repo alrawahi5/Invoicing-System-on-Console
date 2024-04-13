a6b94a8a5642d48f2564e770ed02cf30b128cea6
ABDULLAH <abdullah555523@hotmail.com>
2024-04-13 23:14:00 +0400

    added the class InvoicingSystem

---
+++ InvoicingSystem.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoicingSystem {
+    private static ShopSettings shopSettings;
+    private static List<Item> items = new ArrayList<>();
+    private static List<Invoice> invoices = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+    }
+}
