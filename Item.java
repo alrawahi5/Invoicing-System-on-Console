6982d3231776c44ce11d7a50887d74f0a1cb0c05
ABDULLAH <abdullah555523@hotmail.com>
2024-04-13 19:39:59 +0400

    added the class Items

---
+++ Item.java
@@ -0,0 +1,8 @@
+public class Item {
+        private String itemId;
+        private String itemName;
+        private double unitPrice;
+        private int quantity;
+
+        // add anything else? ...
+}
