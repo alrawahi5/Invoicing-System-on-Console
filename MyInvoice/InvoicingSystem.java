9db9ba238dd2c21ab3d7e74feb73d25d813e04e4
ABDULLAH <abdullah555523@hotmail.com>
2024-04-21 12:01:51 +0400

    added the full finctionality of the Item class

--- MyInvoice/InvoicingSystem.java
+++ MyInvoice/InvoicingSystem.java
@@ -10,9 +10,10 @@ public class InvoicingSystem {
     public static List<Item> items = new ArrayList<>();
     private static List<Invoice> invoices = new ArrayList<>();
     static Invoice invoice1 = new Invoice();
+
     @Override
     public String toString() {
-        for (int i = 0; i<items.size(); i++){
+        for (int i = 0; i < items.size(); i++) {
             items.get(i).toString();
         }
         return items.toString();
@@ -29,22 +30,21 @@ public class InvoicingSystem {
         String phoneNumber = scanner.nextLine();
         Menue.invoice1.phoneNumber = phoneNumber;
 
-        System.out.println("What are you buying? When you are done, type " + "done ");
         while (true) {
+            System.out.println("What are you buying? When you are done, type " + "done ");
             String input = scanner.nextLine();
             if (input.equals("done")) {
                 break;
             }
             Item obj = new Item();
             obj.itemName = input;
-            obj.itemId = (int)(Math.random() * (253 - 1 + 1) + 1);
-            obj.unitPrice = (int)(Math.random() * (5 - 1 + 1) + 1);
+            obj.itemId = (int) (Math.random() * (253 - 1 + 1) + 1);
+            obj.unitPrice = (int) (Math.random() * (5 - 1 + 1) + 1);
+            System.out.println("How many " + obj.itemName + " do you want? ");
+            obj.quantity = scanner.nextInt();
+            scanner.nextLine();
 
-            for(int i = 0; i<items.size(); i++){
-                System.out.println("How many " + input +" do you want? ");
-                obj.quantity = scanner.nextInt();
 
-            }
 
             items.add(obj);
         }
