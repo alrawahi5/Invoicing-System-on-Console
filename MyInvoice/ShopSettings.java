030a598cadc34e55c86ebc652d56ed6165acfbf2
ABDULLAH <abdullah555523@hotmail.com>
2024-04-22 20:41:17 +0400

    added geters and setters for the ShopSettings class

--- MyInvoice/ShopSettings.java
+++ MyInvoice/ShopSettings.java
@@ -6,4 +6,20 @@ public class ShopSettings {
     public String shopName;
     private String invoiceHeader;
     //... add something?
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getInvoiceHeader() {
+        return invoiceHeader;
+    }
+
+    public void setInvoiceHeader(String invoiceHeader) {
+        this.invoiceHeader = invoiceHeader;
+    }
 }
